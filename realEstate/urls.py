fabc234316e81969553eddf61ad63756a978030c
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2024-11-23 12:13:07 +0330

    create statics folder

--- realEstate/urls.py
+++ realEstate/urls.py
@@ -23,3 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('homepage.urls'))
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
